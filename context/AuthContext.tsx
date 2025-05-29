4cc5983ef2e07263e52f9a85134be6ed78f14a95
Revenant-fivem <59361858+Revenant-discord@users.noreply.github.com>
2025-05-29 13:04:53 +0200

    asd

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, FC, ReactNode } from 'react';
+import React, { createContext, useState, useEffect, FC, ReactNode } from 'react';
 
 interface User {
   email: string;
@@ -6,28 +6,41 @@ interface User {
 
 interface AuthContextType {
   user: User | null;
+  isInitialLoading: boolean; // pouze pro inicializaci aplikace!
   login: (email: string, password: string) => Promise<void>;
   logout: () => void;
 }
 
 export const AuthContext = createContext<AuthContextType>({
   user: null,
+  isInitialLoading: true,
   login: async () => {},
   logout: () => {},
 });
 
 const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
+  const [isInitialLoading, setIsInitialLoading] = useState(true);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      setIsInitialLoading(true);
+      await new Promise((res) => setTimeout(res, 1600));
+      setUser(null); // nebo načíst uživatele z úložiště
+      setIsInitialLoading(false);
+    };
+    checkSession();
+  }, []);
 
   const login = async (email: string, password: string) => {
-    // ZDE můžeš řešit ověření uživatele (zatím fake login)
+    // ZDE můžeš řešit ověření uživatele (případný loading řeš v LoginScreen)
     setUser({ email });
   };
 
   const logout = () => setUser(null);
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, isInitialLoading, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
